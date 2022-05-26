f95c054413841385c252f9097fbcfa07b37b3876
Dev Patel <devpatel2556@gmail.com>
2022-05-26 15:56:50 -0400

    added pagination

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,8 +1,11 @@
-import { Button, Card, Stack, Modal} from "react-bootstrap"
+import { Button, Card, Stack, Modal, Pagination, Row, Col, Container } from "react-bootstrap"
+import { BsChevronDoubleLeft, BsChevronDoubleRight, BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { IconContext } from "react-icons"
 import { useState } from "react"
 import ProjectCard from "./ProjectCard"
 import "./component-styles/ProjectContainer.css"
 import ProjectForm from "./ProjectForm"
+
 
 const ProjectContainer = () => {
 
@@ -10,12 +13,13 @@ const ProjectContainer = () => {
     const [projects, setProjects] = useState([])
     const [ID, setID] = useState(1)
     const [deleteID, setDeleteID] = useState(0)
+    const [page, setPage] = useState(0)
 
     const handleShow = () => setShow(true)
     const handleClose = () => setShow(false)
-    
+
     const handleDelete = () => {
-        let ind = 0 
+        let ind = 0
         for (let project of projects) {
             if (project.id === deleteID) {
                 projects.splice(ind, 1)
@@ -23,27 +27,98 @@ const ProjectContainer = () => {
                 setProjects([...projects])
                 break
             }
-    
+
             ind += 1
         }
     }
 
+    const handlePage = (direction) => {
+        if (direction === "fast-forward") {
+            const rem = projects.length % 3 
+
+            if (projects.length !== 0 && rem === 0) {
+                setPage(projects.length - 3)
+            }
+
+            else {
+                setPage(projects.length - rem)
+            }
+        }
+        
+        else if (direction === "fast-backward") {
+            setPage(0)
+        }
+
+        else if (direction === "forward") { 
+            if (page + 3 < projects.length) {      
+                setPage(currentPage => currentPage + 3)
+            }
+        }
+
+        else if (direction === "back") { 
+            if (page - 3 >= 0) {      
+                setPage(currentPage => currentPage - 3)
+            }
+        }
+
+    }
+
     return (
-            <Stack>
-                <Card.Header className="project-header">Projects
-                    <Button className="project-btn" onClick={handleShow}>Add Project</Button>
-                </Card.Header>
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        Create Project
-                    </Modal.Header>
-                    <Modal.Body>
-                        <ProjectForm projects={projects} setProjects={setProjects} handleClose={handleClose} ID={ID} setID={setID}></ProjectForm>
-                    </Modal.Body>
-                </Modal>
-                {deleteID ? handleDelete() : null}
-                {projects ? projects.map((project) => <ProjectCard key={project.id} project={project} setDeleteID={setDeleteID}></ProjectCard>) : null}
-            </Stack>
+        <Stack>
+            <Card.Header className="project-header">Projects
+                <Button className="project-btn" onClick={handleShow}>Add Project</Button>
+            </Card.Header>
+            <Container className="page-cont">
+                <Row className="page-row">
+                    <Col className="page-col">
+                        <Pagination className="page-left">
+                            <Pagination.Item onClick={() => handlePage("fast-backward")}>
+                                <IconContext.Provider value={{ color: "white", size: 35 }}>
+                                    <BsChevronDoubleLeft></BsChevronDoubleLeft>
+                                </IconContext.Provider>
+                            </Pagination.Item>
+                        </Pagination>
+                    </Col>
+                    <Col className="page-col">
+                        <Pagination className="page-left">
+                            <Pagination.Item onClick={() => handlePage("back")}>
+                                <IconContext.Provider value={{ color: "white", size: 35 }}>
+                                    <BsChevronLeft></BsChevronLeft>
+                                </IconContext.Provider>
+                            </Pagination.Item>
+                        </Pagination>
+                    </Col>
+                    <Col className="page-col">
+                        <Pagination className="page-right">
+                            <Pagination.Item onClick={() => handlePage("forward")}>
+                                <IconContext.Provider value={{ color: "white", size: 35 }}>
+                                    <BsChevronRight></BsChevronRight>
+                                </IconContext.Provider>
+                            </Pagination.Item>
+                        </Pagination>
+                    </Col>
+                    <Col className="page-col">
+                        <Pagination className="page-right">
+                            <Pagination.Item onClick={() => handlePage("fast-forward")}>
+                                <IconContext.Provider value={{ color: "white", size: 35}}>
+                                    <BsChevronDoubleRight></BsChevronDoubleRight>
+                                </IconContext.Provider>
+                            </Pagination.Item>
+                        </Pagination>
+                    </Col>
+                </Row>
+            </Container>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    Create Project
+                </Modal.Header>
+                <Modal.Body>
+                    <ProjectForm projects={projects} setProjects={setProjects} handleClose={handleClose} ID={ID} setID={setID}></ProjectForm>
+                </Modal.Body>
+            </Modal>
+            {deleteID ? handleDelete() : null}
+            {projects ? projects.slice(page, page+3).map((project) => <ProjectCard key={project.id} project={project} setDeleteID={setDeleteID}></ProjectCard>) : null}
+        </Stack>
     )
 
 }
